fe40b356edf394bd4e403172a03d31105f35d6d4
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-09-12 22:34:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Vanilla fontFlex
+> VanillaJS extension (based on [jQuery.fontFlex](https://github.com/nathco/jQuery.fontFlex)) for dynamically changing font sizes according to container / browser width. Intended for use with responsive or adaptive CSS layouts.
+
+## Installation
+Include the `fontFlex.min.js` before the `</body>` of your HTML document:
+```html
+<script src="fontFlex.min.js"></script>
+```
+
+## How to Use
+Define a default `CSS` font base by setting `font-size: 1em` and `line-height: 150%` on the `body` or intended element. Declaring the `font-size` is optional, but highly recommended in case javascript is disabled. Finally, call the plugin on said element.
+
+**Syntax Example**
+```javascript
+FontFlex.init({
+    min: 20,
+    max: 60,
+    mid: 40,
+    target: 'h1'
+});
+```
+
+**Custom Parameters**
+`min` Minimum font-size in pixels
+`max` Maximum font-size in pixels
+`mid` Mid-range buffer. Values ranging from 60 to 70 produce the best results. Lower values produce a larger initial font-size, while higher values produce the opposite. Adjust accordingly to fit your requirements.
+
+## Browser support
+
+![IE](https://cloud.githubusercontent.com/assets/398893/3528325/20373e76-078e-11e4-8e3a-1cb86cf506f0.png) | ![Chrome](https://cloud.githubusercontent.com/assets/398893/3528328/23bc7bc4-078e-11e4-8752-ba2809bf5cce.png) | ![Firefox](https://cloud.githubusercontent.com/assets/398893/3528329/26283ab0-078e-11e4-84d4-db2cf1009953.png) | ![Opera](https://cloud.githubusercontent.com/assets/398893/3528330/27ec9fa8-078e-11e4-95cb-709fd11dac16.png) | ![Safari](https://cloud.githubusercontent.com/assets/398893/3528331/29df8618-078e-11e4-8e3e-ed8ac738693f.png)
+--- | --- | --- | --- | --- |
+IE 9+ ✔ | Latest ✔ | Latest ✔ | Latest ✔ | Latest ✔ |
+
+## Contributing
+
+Check [CONTRIBUTING.md](CONTRIBUTING.md) for more information.
+
+## History
+
+Check [Releases](https://github.com/pinceladasdaweb/fontFlex/releases) for detailed changelog.
+
+## License
+
+[MIT](LICENSE)
---
+++ src/fontFlex.js
@@ -0,0 +1,77 @@
+/*jslint browser: true*/
+/*global define, module, exports*/
+(function (root, factory) {
+    'use strict'; // For jslint
+
+    // AMD. Register as an anonymous module. Wrap in function so we have access
+    // to root via `this`.
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+
+    // Node. Does not work with strict CommonJS, but only CommonJS-like
+    // environments that support module.exports, like Node.
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+
+    // Browser globals.
+    } else {
+        root.FontFlex = factory();
+    }
+}(this, function () {
+    'use strict';
+
+    var FontFlex = function (options) {
+        if (!this || !(this instanceof FontFlex)) {
+            return new FontFlex(options);
+        }
+
+        if (!options.target) {
+            throw new TypeError('Provide a target to apply FontFlex');
+        }
+
+        if (typeof options === 'string') {
+            options = { key : options };
+        }
+
+        this.target = Array.prototype.slice.call(document.querySelectorAll(options.target), 0);
+        this.min    = options.min;
+        this.max    = options.max;
+        this.mid    = options.mid;
+
+        this.ready();
+    };
+
+    FontFlex.init = function (options) {
+        return new FontFlex(options);
+    };
+
+    FontFlex.prototype = {
+        trigger: function (eventName) {
+            var event = document.createEvent('HTMLEvents');
+            event.initEvent(eventName, true, false);
+
+            window.dispatchEvent(event);
+        },
+        ready: function () {
+            window.addEventListener('resize', function () {
+                var size = window.innerWidth / this.mid;
+
+                if (size < this.min) {
+                    size = this.min;
+                }
+
+                if (size > this.max) {
+                    size = this.max;
+                }
+
+                this.target.forEach(function (els) {
+                    els.style.fontSize = size + 'px';
+                });
+            }.bind(this), false);
+
+            this.trigger('resize');
+        }
+    };
+
+    return FontFlex;
+}));
